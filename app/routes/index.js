c2397d183362e954b10b2ec72af1a39c2ff51e40
joeluckett97 <joe.luckett@eagles.oc.edu>
2026-02-04 07:30:59 -0600

    Completed code for the task status backend table

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,13 @@
 import { Router } from "express";
 
 import AuthRoutes from "./auth.routes.js";
-import UserRoutes from "./user.routes.js";
-import TutorialRoutes from "./tutorial.routes.js";
-import LessonRoutes from "./lesson.routes.js";
+import TaskStatus from "./taskStatus.routes.js";
 
 
 const router = Router();
 
 router.use("/", AuthRoutes);
-router.use("/users", UserRoutes);
-router.use("/tutorials", TutorialRoutes);
-router.use("/tutorials", LessonRoutes);
+router.use("/taskStatus", TaskStatus);
+
 
 export default router;
