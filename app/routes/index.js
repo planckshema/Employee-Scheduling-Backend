ca9f56b64131737129b0444c075a11349c6f0d5f
joeluckett97 <joe.luckett@eagles.oc.edu>
2026-02-04 07:36:20 -0600

    Completed code for the shift swap request backend table

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,13 @@
 import { Router } from "express";
 
 import AuthRoutes from "./auth.routes.js";
-import UserRoutes from "./user.routes.js";
-import TutorialRoutes from "./tutorial.routes.js";
-import LessonRoutes from "./lesson.routes.js";
+import ShiftSwapRequestRoutes from "./shiftSwapRequest.routes.js";
 
 
 const router = Router();
 
 router.use("/", AuthRoutes);
-router.use("/users", UserRoutes);
-router.use("/tutorials", TutorialRoutes);
-router.use("/tutorials", LessonRoutes);
+router.use("/shiftSwapRequests", ShiftSwapRequestRoutes);
+
 
 export default router;
