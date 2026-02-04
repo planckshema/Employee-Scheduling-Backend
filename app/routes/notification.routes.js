607250b198117cfd4e9e52ce611ada4ae346a335
joeluckett97 <joe.luckett@eagles.oc.edu>
2026-02-04 06:47:53 -0600

    Completed code for the notification table on the backend. ready for testing

---
+++ app/routes/notification.routes.js
@@ -0,0 +1,11 @@
+  import notifications from "../controllers/notifications.controller.js";
+  import  authenticate from "../authorization/authorization.js";
+  import { Router } from "express";
+  var router = Router()
+
+
+  // Create a new User
+  router.post("/", [authenticate], notifications.create);
+
+  export default router;
+
