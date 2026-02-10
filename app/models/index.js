92700047458eda7529374fbd8dcd2bcacbe1445c
ButterR26 <122320580+RyanRuehle@users.noreply.github.com>
2026-02-10 13:26:18 -0600

    Create Availability table
    Create Availability table

--- app/models/index.js
+++ app/models/index.js
@@ -8,6 +8,8 @@ import User from "./user.model.js";
 import Session from "./session.model.js";
 import Tutorial from "./tutorial.model.js";
 import Lesson from "./lesson.model.js"; 
+import EmployeeAvailability from "./employeeAvailability.js";
+
 
 
 const db = {};
@@ -18,6 +20,7 @@ db.user = User;
 db.session = Session;
 db.tutorial = Tutorial;
 db.lesson = Lesson;
+db.employeeAvailability = EmployeeAvailability;
 
 // foreign key for session
 db.user.hasMany(
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,8 @@ import AuthRoutes from "./auth.routes.js";
 import UserRoutes from "./user.routes.js";
 import TutorialRoutes from "./tutorial.routes.js";
 import LessonRoutes from "./lesson.routes.js";
+import EmployeeAvailabilityRoutes from "./employeeAvailability.routes.js";
+
 
 
 const router = Router();
@@ -12,5 +14,7 @@ router.use("/", AuthRoutes);
 router.use("/users", UserRoutes);
 router.use("/tutorials", TutorialRoutes);
 router.use("/tutorials", LessonRoutes);
+router.use("/", EmployeeAvailabilityRoutes);
+
 
 export default router;
