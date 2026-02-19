8c49f4f9f64288887680c3039197e38ccd7f04aa
joeluckett97 <joe.luckett@eagles.oc.edu>
2026-02-19 15:12:47 -0600

    Seed file updated

--- app/controllers/shift.controller.js
+++ app/controllers/shift.controller.js
@@ -1,45 +1,57 @@
-import db  from "../models/index.js";
+import db from "../models/index.js";
 import logger from "../config/logger.js";
 
 const Shift = db.shift;
 const Op = db.Sequelize.Op;
 const exports = {};
 
-
-// This next code is only here to serve as an example for when we are ready to write code to query the database, can be deleted or modified
-
-
-// Create and Save a new Tutorial
+// Create and Save a new Shift
 exports.create = (req, res) => {
-  // Validate request
-  if (!req.body.title) {
-    logger.warn('Tutorial creation attempt with empty title');
+  // Validate request (using day or startTime as a required field)
+  if (!req.body.startTime || !req.body.startDate) {
+    logger.warn('Shift creation attempt with missing time/date');
     res.status(400).send({
-      message: "Content can not be empty!",
+      message: "Shift startTime and startDate cannot be empty!",
     });
     return;
   }
-  // Create a Tutorial
-  const tutorial = {
-    title: req.body.title,
-    description: req.body.description,
-    published: req.body.published ? req.body.published : false,
-    userId: req.body.userId,
+
+  // Create a Shift object based on your model
+  const shift = {
+    day: req.body.day,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+    startDate: req.body.startDate
   };
-  
-  logger.debug(`Creating tutorial: ${tutorial.title} for user: ${tutorial.userId}`);
-  
-  // Save Tutorial in the database
-  Tutorial.create(tutorial)
+
+  logger.debug(`Creating shift for day: ${shift.day} starting at: ${shift.startTime}`);
+
+  // Save Shift in the database
+  Shift.create(shift)
     .then((data) => {
-      logger.info(`Tutorial created successfully: ${data.id} - ${data.title}`);
+      logger.info(`Shift created successfully: ID ${data.shiftId}`);
       res.send(data);
     })
     .catch((err) => {
-      logger.error(`Error creating tutorial: ${err.message}`);
+      logger.error(`Error creating shift: ${err.message}`);
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Tutorial.",
+        message: err.message || "Some error occurred while creating the Shift.",
+      });
+    });
+};
+
+// Retrieve all Shifts from the database (This is what your Dashboard uses)
+exports.findAll = (req, res) => {
+  logger.debug("Fetching all shifts for the dashboard");
+
+  Shift.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      logger.error(`Error retrieving shifts: ${err.message}`);
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving shifts.",
       });
     });
 };
--- app/models/index.js
+++ app/models/index.js
@@ -46,4 +46,73 @@ db.taskStatus = TaskStatus;
 db.shiftSwapRequest = ShiftSwapRequest;
 db.shift = Shift;
 
+
+// --- Relationships / Associations ---
+
+// 1. Employee & User (One-to-One)
+db.user.belongsTo(db.employee, { foreignKey: 'EmployeeID' });
+db.employee.hasOne(db.user, { foreignKey: 'EmployeeID' });
+
+// 2. Employer & BusinessArea (One-to-Many)
+db.employer.hasMany(db.businessArea, { foreignKey: 'EmployerID' });
+db.businessArea.belongsTo(db.employer, { foreignKey: 'EmployerID' });
+
+// 3. BusinessArea & Positions (One-to-Many)
+db.businessArea.hasMany(db.position, { foreignKey: 'AreaID' });
+db.position.belongsTo(db.businessArea, { foreignKey: 'AreaID' });
+
+// 4. BusinessArea & Schedule (One-to-Many)
+db.businessArea.hasMany(db.schedule, { foreignKey: 'AreaID' });
+db.schedule.belongsTo(db.businessArea, { foreignKey: 'AreaID' });
+
+// 5. Employee & EmployeePosition (Many-to-Many via EmployeePosition)
+db.employee.hasMany(db.employeePosition, { foreignKey: 'EmployeeID' });
+db.employeePosition.belongsTo(db.employee, { foreignKey: 'EmployeeID' });
+db.position.hasMany(db.employeePosition, { foreignKey: 'PositionID' });
+db.employeePosition.belongsTo(db.position, { foreignKey: 'PositionID' });
+
+// 6. Shift Relationships
+// Shift belongs to Employee, Schedule, and TaskList
+db.employee.hasMany(db.shift, { foreignKey: 'EmployeeID' });
+db.shift.belongsTo(db.employee, { foreignKey: 'EmployeeID' });
+
+db.schedule.hasMany(db.shift, { foreignKey: 'ScheduleID' });
+db.shift.belongsTo(db.schedule, { foreignKey: 'ScheduleID' });
+
+db.taskList.hasMany(db.shift, { foreignKey: 'TaskListID' });
+db.shift.belongsTo(db.taskList, { foreignKey: 'TaskListID' });
+
+// 7. Clock In/Out & Shift
+db.shift.hasMany(db.clockInTime, { foreignKey: 'ShiftID' });
+db.clockInTime.belongsTo(db.shift, { foreignKey: 'ShiftID' });
+
+db.shift.hasMany(db.clockOutTime, { foreignKey: 'ShiftID' });
+db.clockOutTime.belongsTo(db.shift, { foreignKey: 'ShiftID' });
+
+// 8. TimeOff & Availability
+db.employee.hasMany(db.timeOff, { foreignKey: 'EmployeeID' });
+db.timeOff.belongsTo(db.employee, { foreignKey: 'EmployeeID' });
+
+db.employee.hasMany(db.employeeAvailability, { foreignKey: 'EmployeeID' });
+db.employeeAvailability.belongsTo(db.employee, { foreignKey: 'EmployeeID' });
+
+// 9. TaskList & TaskListItems
+db.taskList.hasMany(db.taskListItem, { foreignKey: 'TaskListID' });
+db.taskListItem.belongsTo(db.taskList, { foreignKey: 'TaskListID' });
+
+// 10. Task Status Tracking
+db.shift.hasMany(db.taskStatus, { foreignKey: 'ShiftID' });
+db.taskStatus.belongsTo(db.shift, { foreignKey: 'ShiftID' });
+
+db.taskListItem.hasMany(db.taskStatus, { foreignKey: 'TaskListItemID' });
+db.taskStatus.belongsTo(db.taskListItem, { foreignKey: 'TaskListItemID' });
+
+// 11. Shift Swap Requests
+db.shift.hasMany(db.shiftSwapRequest, { foreignKey: 'ShiftID' });
+db.shiftSwapRequest.belongsTo(db.shift, { foreignKey: 'ShiftID' });
+
+// Links for Offering and Accepting Employees
+db.employee.hasMany(db.shiftSwapRequest, { foreignKey: 'EmployeeOfferID', as: 'OfferedSwaps' });
+db.employee.hasMany(db.shiftSwapRequest, { foreignKey: 'EmployeeAcceptID', as: 'AcceptedSwaps' });
+
 export default db;
