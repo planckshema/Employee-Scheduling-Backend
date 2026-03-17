494b922606a1a96b411e9ca773da85ee4a78669b
joeluckett97 <joe.luckett@eagles.oc.edu>
2026-03-17 14:01:21 -0500

    Small changes to get ready to create the template section

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
   
 // Load the routes from the routes folder
-app.use("/shifts", routes); 
+app.use("/worker_scheduling", routes); 
 
 
 // set port, listen for requests
